458799fe3a77b9d9c19a40a3860f87e4d389f0ff
Marcos Camargo <marcos.camargo@mobiup.com.br>
2021-04-15 16:07:56 -0300

    Inlcuindo componente para clientes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Guia clientes</h1>
+    <div>
+      <Clientes></Clientes>
+      <Clientes></Clientes>
+      <Clientes></Clientes>
+      <Clientes></Clientes>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Clientes from './components/Clientes'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'App', 
+  components:{
+    Clientes
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

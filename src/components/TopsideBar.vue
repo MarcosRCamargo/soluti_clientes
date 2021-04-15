a994f4ade2f97234c46c57688b1da3342603bb23
Marcos Camargo <marcos.camargo@mobiup.com.br>
2021-04-15 16:55:49 -0300

    Adicionando topsidebar e biblioteca de componentes UI

---
+++ src/components/TopsideBar.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <MdButton>Botão md</MdButton>
+    <MdTabs>Tab</MdTabs>
+</div>
+    
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+import Vue from 'vue'
+import { MdButton, MdContent, MdTabs } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+
+Vue.use(MdButton)
+Vue.use(MdContent)
+Vue.use(MdTabs)
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style scoped>
+
+</style>

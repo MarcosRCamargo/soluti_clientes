4a00de05e0f00603e8818fe0b61bc7cdd9de40f7
Marcos Camargo <marcos.camargo@mobiup.com.br>
2021-04-15 16:35:21 -0300

    Adicionando componentes de Endereço e Certificado

---
+++ src/components/Clientes.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="clientes">
+        <h2 id="cliente_nome">Marcos Camargo</h2>
+        <h3>cpf: </h3>
+        <h3>data de Nascimento: </h3>
+        <h3>telefone: </h3>
+        <hr>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped>
+#clientes{
+    color:rgb(87, 145, 0);
+    background-color:aquamarine;
+}
+#cliente_nome{
+    color:rgb(53, 53, 53);
+}
+</style>>

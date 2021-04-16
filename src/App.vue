b4cee1a7079ffc8e122f551abc5382c72594375d
Marcos Camargo <marcos.camargo@mobiup.com.br>
2021-04-16 16:52:19 -0300

    Atualizando projeto com Rotas

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,39 @@
   <div id="app">
     <TopsideBar :userIsLoged="false"></TopsideBar>
     <h1>Guia clientes</h1>
-    <div>
-      <Clientes></Clientes>
-      <Clientes></Clientes>
-      <Clientes></Clientes>
+    <div fluid="lg">
+      <b-container class="bv-client-row" align-v="start">
+        <b-row class="text-center" align-v="start">
+          <b-col cols="auto" class="mr-auto">
+            <b-list-group>
+              <b-list-group-item>Cras justo odio</b-list-group-item>
+              <b-list-group-item active
+                >Dapibus ac facilisis in</b-list-group-item
+              >
+              <b-list-group-item>Morbi leo risus</b-list-group-item>
+              <b-list-group-item>Porta ac consectetur ac</b-list-group-item>
+              <b-list-group-item>Vestibulum at eros</b-list-group-item>
+            </b-list-group>
+          </b-col>
+          <b-col cols="9" class="bg-info fluid">2 of 3 (wider)</b-col>
+        </b-row>
+        <b-row class="text-center">
+          <b-col>@copy</b-col>
+        </b-row>
+      </b-container>
       <Clientes></Clientes>
     </div>
   </div>
 </template>
-
 <script>
-
-import Clientes from './components/Clientes'
-import TopsideBar from './components/TopsideBar'
-
+import Clientes from "./components/Clientes";
+import TopsideBar from "./components/TopsideBar";
 export default {
-  name: 'App', 
-  components:{
+  name: "App",
+  components: {
     Clientes,
-    TopsideBar
-  }
-}
+    TopsideBar,
+  },
+};
 </script>
-
-<style>
-
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from "bootstrap-vue";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 // Make BootstrapVue available throughout your project
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(BootstrapVueIcons)
+Vue.use(BootstrapVueIcons);
 
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");

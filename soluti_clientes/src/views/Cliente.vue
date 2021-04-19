dd589875f6f50a89617dd75739ade6d00b1a7214
Marcos Camargo <marcos.camargo@mobiup.com.br>
2021-04-19 01:07:49 -0300

    Atualizando Página de clientes consumindo API

--- soluti_clientes/src/views/Cliente.vue
+++ soluti_clientes/src/views/Cliente.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="client">
     <h1>This is an client page</h1>
+    <table>
+      <tbody>
+        <tr v-for="cliente of clientes" :key="cliente.id">
+          <td>cliente.name</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
+
+<script>
+import Clientes  from '../services/clientes'
+
+export default ({
+  data(){
+    return{
+    clientes:[]
+    }
+
+  },
+  mounted() {
+    Clientes.listar().then(response =>{
+      console.log(response.data)
+      this.clientes = response.data
+    })
+  },
+})
+</script>
--- soluti_clientes/src/views/Home.vue
+++ soluti_clientes/src/views/Home.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="home">
-    <HelloSoluti msg="Bem vindo a Gestão de Clientes Sis-Soluti"/>
-    <p>Usuários do sistema:<p/>
+    <HelloSoluti msg="Bem vindo a Gestão de Clientes Sis-Soluti" />
+    <p>Usuários do sistema:</p>
+    <p />
     <div class="mb-4">
       <b-avatar></b-avatar>
       <b-avatar variant="primary" text="BV"></b-avatar>
       <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
       <b-avatar variant="success" icon="people-fill"></b-avatar>
     </div>
-    <b-list-group style="max-width: 100%;" class="fluid p-5 mb-4" >
+    <b-list-group
+      v-for="cliente of clientes"
+      :key="cliente.id"
+      style="max-width: 100%"
+      class="fluid p-5 mb-4"
+    >
       <b-list-group-item class="d-flex align-items-center">
         <b-avatar variant="success" icon="people" class="mr-3"></b-avatar>
-        <span class="mr-auto">Marcos Camargo</span>
+        <span class="mr-auto">{{ cliente.nome }}</span>
         <span class="mr-auto">@Certificado</span>
-        <span >Verificação: <b-badge variant="success">valido</b-badge></span>
+        <span>Verificação: <b-badge variant="success">valido</b-badge></span>
         <b-icon class="mr-auto" icon="shield-check"></b-icon>
-        <b-badge variant="success">email@mail.com</b-badge>
+        <b-badge variant="success">{{ cliente.email }}</b-badge>
       </b-list-group-item>
     </b-list-group>
   </div>
@@ -23,12 +29,23 @@
 
 <script>
 // @ is an alias to /src
-import HelloSoluti from '@/components/HelloSoluti.vue'
-
+import HelloSoluti from "@/components/HelloSoluti.vue";
+import Cliente from "../services/clientes";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloSoluti
-  }
-}
+    HelloSoluti,
+  },
+  data() {
+    return {
+      clientes: [],
+    };
+  },
+  mounted() {
+    Cliente.listar().then(resposta => {
+      console.log(resposta.data);
+      this.clientes = resposta.data
+    });
+  },
+};
 </script>
